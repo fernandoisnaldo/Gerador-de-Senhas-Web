c82242aabed63eabdcf3f1c1792931c9ae0166d5
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-15 09:42:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function setASCII(){
 }
 function setSilabas(){
     tipoElemento = 1;
-    exibirTipo.innerText="Sílabas";
+    exibirTipo.innerText="Sílabas (aviso: pode sair palavras feias por razões da combinação aleatória de fonemas)";
 }
 function setAlfanumerico(){
     tipoElemento = 2;
