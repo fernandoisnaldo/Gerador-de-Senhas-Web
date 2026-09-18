3932aec6201d004e5f30dd0bab702fb037f76c84
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-18 01:33:38 -0300

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# este shellscript é meramente um protótipo
+# este shellscript é meramente um protótipo, não recomendo executar isso para fins práticos
 mkdir -p build/GeradordeSenhas-web
 cp index.html build/GeradordeSenhas-web/index.html
 cp main.js build/GeradordeSenhas-web/main.js
