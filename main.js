2069c976ff3d273e43495fedc56a7385297df135
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-21 13:28:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ function numAleatorio(max) {
 }
 function setASCII(){
     tipoElemento = 0;
+    atualizarAriaPressed(event?.target);
     ascii_span.style.display="inline";
     silabas_span.style.display="none";
     alfanum_span.style.display="none";
@@ -66,6 +67,7 @@ function setASCII(){
 }
 function setSilabas(){
     tipoElemento = 1;
+    atualizarAriaPressed(event?.target);
     ascii_span.style.display="none";
     silabas_span.style.display="inline";
     alfanum_span.style.display="none";
@@ -75,6 +77,7 @@ function setSilabas(){
 }
 function setAlfanumerico(){
     tipoElemento = 2;
+    atualizarAriaPressed(event?.target);
     ascii_span.style.display="none";
     silabas_span.style.display="none";
     alfanum_span.style.display="inline";
@@ -84,6 +87,7 @@ function setAlfanumerico(){
 }
 function setHexadecimal(){
     tipoElemento = 3;
+    atualizarAriaPressed(event?.target);
     ascii_span.style.display="none";
     silabas_span.style.display="none";
     alfanum_span.style.display="none";
@@ -92,12 +96,19 @@ function setHexadecimal(){
 }
 function setDecimal(){
     tipoElemento = 4;
+    atualizarAriaPressed(event?.target);
     ascii_span.style.display="none";
     silabas_span.style.display="none";
     alfanum_span.style.display="none";
     hexadecimal_span.style.display="none";
     numero_span.style.display="inline";
-
+}
+function atualizarAriaPressed(botaoClicado) {
+    let botoes = document.querySelectorAll('[id="tipo_senha"]');;
+    botoes.forEach(btn => btn.setAttribute("aria-pressed", "false"));
+    if (botaoClicado) {
+        botaoClicado.setAttribute("aria-pressed", "true");
+    }
 }
 function gerarSenha(){
     senha.innerText="";
