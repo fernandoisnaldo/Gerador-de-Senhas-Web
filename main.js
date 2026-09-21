24c6a6c05c458d38ae94d20bc771413cf7d33378
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-20 21:10:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -160,7 +160,9 @@ function copiar(){
         .catch(erro => alert("Clipboard object: " + erro));
 }
 function inicializa(){
-    senha.innerText="Powered by Web Crypto API";//substitui o aviso padrão de "requer JavaScript" que está em outros documentos HTML.
+    if (window.crypto && window.crypto.getRandomValues) {
+        senha.innerText = "Powered by Web Crypto API";
+    }
     for (let c = 0; c < alfabeto.consoantes.length; c++) {
         for (let v = 0; v < alfabeto.vogais.length; v++) {
             for (let t = 0; t < alfabeto.terminacoes.length; t++) {
