04df24a260fe27fad5b26ff5cc07ee12377a1ae3
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-22 15:46:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -150,6 +150,11 @@ function gerarSenha(){
     }
     senha.innerText=novaSenha.join("");
     //sorteio de palheta de cores
+    let testeUnitario = {};
+        for (let caractere of novaSenha) {
+        testeUnitario[caractere] = (testeUnitario[caractere] || 0) + 1;
+    }
+    console.log("Frequência dos elementos:", testeUnitario);
     senha.style.backgroundColor="#" + numAleatorio(3) + numAleatorio(3) + numAleatorio(3);
     senha.style.color="#" + numAleatorio(16).toString(16) + (numAleatorio(5)+11).toString(16) + numAleatorio(16).toString(16);
     senha.style.border="#" + numAleatorio(16).toString(16) + (numAleatorio(7)+9).toString(16) + numAleatorio(16).toString(16) + " dashed 1px";
