5cba1794ea66945bda0d53b530d5fe6c61017884
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-24 05:30:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -201,6 +201,7 @@ function gerarSenha(){
         }
     } else {
         console.log("Entropy: 0.00 bits");
+        exibirCopiar.style.display="none";
     }
     console.log(testeDistr);
     const frequencias = Object.values(testeDistr);
