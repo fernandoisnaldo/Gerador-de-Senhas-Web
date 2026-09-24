af0e804bf48227d84d383eaeb460970d2b4f0b8e
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-24 05:21:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -188,8 +188,11 @@ function gerarSenha(){
         else if(totalEntropy<192){
             botaoCopiarIndice=1; // senha nível aceitável
         }
-        else {
+        else  if(totalEntropy>=192{
             botaoCopiarIndice=2; // senha forte
+        }
+        else {
+            botaoCopiarIndice=4; // índice fantasma previnir que o botão copiar apareça indevidamente
         }
         for(let contador=0; contador<botoes_copiar.length;contador++){
             if(contador== botaoCopiarIndice){
