3eaf8e0bae564cc9959264ecdff3d44d0dfd8895
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-25 06:39:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -55,13 +55,13 @@ let alfabeto = {
     ],
     silabas: []//este array vai ser ocupado depois que o código inicializar
 };
-const BCSPRNG = new Uint32Array(1); //Buffer pra uso do CSPRNG
+const bufferAleatorio = new Uint32Array(1); //Buffer pra uso do CSPRNG
 function numAleatorio(max) {
     const maxPermitido = Math.floor(4294967296 / max) * max;
     let valor;
     do {
-        window.crypto.getRandomValues(BCSPRNG);
-        valor = BCSPRNG[0];
+        window.crypto.getRandomValues(bufferAleatorio);
+        valor = bufferAleatorio[0];
     } while (valor >= maxPermitido); //rejeita viés de módulo
     return valor % max;
 }
