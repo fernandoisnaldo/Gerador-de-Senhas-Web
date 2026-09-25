9e9b8edcf85e4fbbcb6f932dc70a3d827d8f6837
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-24 23:54:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -224,9 +224,9 @@ function gerarSenha(){
 function filtro(c, v, t) { // filtro contra opressão
     if(c===19 && v===1 && t===51) return false;
     else if(c===14 && v===0 && t===10) return false;
-    else if((c===14 && v===0 && t===11)) return false;
-    else if((c===39 && v===17 && t===10)) return false;
-    else if((c===39 && v===17 && t===11)) return false;
+    else if(c===14 && v===0 && t===11) return false;
+    else if(c===39 && v===17 && t===10) return false;
+    else if(c===39 && v===17 && t===11) return false;
     else if(c===5 && v===27 && t===20) return false;
     else if(c===19 && v===27 && t===15) return false;
     return true;
