854f023612ef10d31a0a27c3921bef04b3afce58
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-26 05:54:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let ascii_span = document.getElementById("tipo_ascii"); //span que muda em tempo
 let silabas_span = document.getElementById("tipo_silabas"); //span aviso que fica invisível quando não tá no módulo sílaba
 let eff_span = document.getElementById("eff_cc");
 let botoes_senha = document.querySelectorAll('[id="tipo_senha"]');
-/Objeto para facilitar a manutenção dos botões de seleção
+//Objeto para facilitar a manutenção dos botões de seleção
 const OPCAO = Object.freeze({ASCII:0, SILABA:1 ,PALAVRA:2, ALFANUM:3, HEX:4, NUM:5, BASE64:6});
 const QtdePADRAO = Object.freeze({CARACTERE:32, DECIMAL:40, PALAVRA:12});
 let separadorFrase = " ";
