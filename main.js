05fc17436424be567d46ef35eb0457877a52c5bb
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-26 17:39:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -271,7 +271,7 @@ function copiar(){
     .catch(erro => alert("Clipboard object: " + erro));
 }
 const encoder = new TextEncoder();
- async function filtro(c, v, t) { // filtro de palavras proibidas pro gerador de sílabas, em sha256
+async function filtro(c, v, t) { // filtro de palavras proibidas pro gerador de sílabas, em sha256
     const buffer = await crypto
     .subtle.digest('SHA-256', encoder.encode(alfabeto.consoantes[c] + alfabeto.vogais[v] + alfabeto.terminacoes[t]));
     const hash = Array.from(new Uint8Array(buffer)).map(b => b.toString(16).padStart(2, '0')).join('');
@@ -280,7 +280,8 @@ const encoder = new TextEncoder();
     else if (hash === "038c9ccdd226f5728bd0a945bdbb0a25c0f877f2f36f4092ee8c004e810aa300") return;
     else if (hash === "7d2969e37aa4ff6030ee5b5b9e60f8689a5bab0a4a24b432d7ee4be157e5f6bd") return;
     else if (hash === "cc02032349c833ac5e97bac094560ed40e09acf34cb1978ab7a9840b9bf15b4d") return;
-    alfabeto.silabas.push(alfabeto.consoantes[c] + alfabeto.vogais[v] + alfabeto.terminacoes[t]);
+    return true;
+    
 }
 async function inicializa(){
     if (window.crypto && window.crypto.getRandomValues) {
@@ -289,7 +290,9 @@ async function inicializa(){
     for (let c = 0; c < alfabeto.consoantes.length; c++) {
         for (let v = 0; v < alfabeto.vogais.length; v++) {
             for (let t = 0; t < alfabeto.terminacoes.length; t++) {
-                await filtro(c,v,t);
+                if(await filtro(c,v,t)){
+                    alfabeto.silabas.push(alfabeto.consoantes[c] + alfabeto.vogais[v] + alfabeto.terminacoes[t]);
+                }
             }
         }
     }
