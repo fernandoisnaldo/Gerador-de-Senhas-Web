03b7ec2170f10efb3402cfd48eefa11b4a7310e6
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-26 00:12:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -219,14 +219,17 @@ function gerarSenha(){
         if(totalEntropy<128){
             botaoCopiarIndice=0; // senha fraca
             senha.style.filter = "grayscale(80%)";
+            botoes_copiar.style.textDecoration = "red underline"; 
         }
         else if(totalEntropy<192){
             botaoCopiarIndice=1; // senha nível aceitável
             senha.style.filter = "grayscale(0%)";
+            botoes_copiar.style.textDecoration = "none"; 
         }
         else  if(totalEntropy>=192){
             botaoCopiarIndice=2; // senha forte
             senha.style.filter = "grayscale(0%)";
+            botoes_copiar.style.textDecoration = "green underline"; 
         }
         for(let contador=0; contador<botoes_copiar.length;contador++){
             if(contador== botaoCopiarIndice){
